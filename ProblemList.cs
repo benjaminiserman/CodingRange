05f08cd23823882477be7225a1b1be39444c932f
Benjamin E. Iserman <winggar1228@gmail.com>
2021-10-27 10:30:06 -0500

    added a few more problems

--- ProblemList.cs
+++ ProblemList.cs
@@ -6,11 +6,45 @@ namespace CodingRange
     {
         public static List<Problem> List { get; } = new()
         {
-            new Problem("Hello World", "Return the string \"Hello World!\"", "(no parameters)", "string", new[] { 
+            new Problem("Hello World", "Return the string \"Hello World!\".", "(no parameters)", "string", new[] { 
                 new TestCase(null, "Hello World!")
             }),
-
-
+            new Problem("Doubling", "Return the input multiplied by two.", "int x", "int", new[] {
+                new TestCase(new object[] { 1 }, 2),
+                new TestCase(new object[] { 0 }, 0),
+                new TestCase(new object[] { -7 }, -14),
+                new TestCase(new object[] { 5 }, 10),
+                new TestCase(new object[] { 817 }, 1634),
+                new TestCase(new object[] { 4 }, 8),
+                new TestCase(new object[] { 21 }, 42),
+            }),
+            new Problem("Squares", "Return the input squared (hint: multiplied by itself).", "int x", "int", new[] {
+                new TestCase(new object[] { 1 }, 1),
+                new TestCase(new object[] { 0 }, 0),
+                new TestCase(new object[] { -7 }, 49),
+                new TestCase(new object[] { 5 }, 25),
+                new TestCase(new object[] { 817 }, 667489),
+                new TestCase(new object[] { 4 }, 16),
+                new TestCase(new object[] { 21 }, 441),
+            }),
+            new Problem("Negatives", "Return the inverse of the input (hint: multiplied by -1).", "int x", "int", new[] {
+                new TestCase(new object[] { 1 }, -1),
+                new TestCase(new object[] { 0 }, 0),
+                new TestCase(new object[] { -7 }, 7),
+                new TestCase(new object[] { 5 }, -5),
+                new TestCase(new object[] { 817 }, -817),
+                new TestCase(new object[] { 4 }, -4),
+                new TestCase(new object[] { 21 }, -21),
+            }),
+            new Problem("Order of Operations", "Add one to the input and return that new value multplied by negative two.", "int x", "int", new[] {
+                new TestCase(new object[] { 1 }, -4),
+                new TestCase(new object[] { 0 }, -2),
+                new TestCase(new object[] { -7 }, 12),
+                new TestCase(new object[] { 5 }, -12),
+                new TestCase(new object[] { 817 }, -1636),
+                new TestCase(new object[] { 4 }, -10),
+                new TestCase(new object[] { 21 }, -44),
+            }),
         };
     }
 }
--- Workspace.cs
+++ Workspace.cs
@@ -2,14 +2,14 @@
 {
     #region Controls
 
-    int problemNumber = 0; // The ID of the current problem you're working on. When you've completed your current problem, increment this by one.
+    int problemNumber = 4; // The ID of the current problem you're working on. When you've completed your current problem, increment this by one.
     bool displayProblem = true; // Should CodingRange display problem details?
     bool evaluateAnswer = true; // Should CodingRange run & evaluate Method?
 
     #endregion
 
-    string Method() // This method MUST be called Method (case-sensitive) for CodingRange to pick it up.
+    int Method(int x) // This method MUST be called Method (case-sensitive) for CodingRange to pick it up.
     {
-        return "Example Return String";
+        return x;
     }
 }
