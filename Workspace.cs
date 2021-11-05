73cfad9be7a8a42795eacffaf54996b44c25f743
Benjamin E. Iserman <winggar1228@gmail.com>
2021-11-05 13:07:24 -0500

    clean Workspace.cs

--- Workspace.cs
+++ Workspace.cs
@@ -2,14 +2,14 @@
 {
     #region Controls
 
-    int problemNumber = 13; // The ID of the current problem you're working on. When you've completed your current problem, increment this by one.
+    int problemNumber = 0; // The ID of the current problem you're working on. When you've completed your current problem, increment this by one.
     bool displayProblem = true; // Should CodingRange display problem details?
     bool evaluateAnswer = true; // Should CodingRange run & evaluate Method?
 
     #endregion
 
-    int Method(int x) // This method MUST be called Method (case-sensitive) for CodingRange to pick it up.
+    string Method() // This method MUST be called Method (case-sensitive) for CodingRange to pick it up.
     {
-        return -x;
+        return "Test";
     }
 }
