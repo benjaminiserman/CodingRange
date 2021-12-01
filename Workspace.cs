9449513578efa0b6bc73f1136955da9bc0ff6024
Benjamin E. Iserman <winggar1228@gmail.com>
2021-11-30 18:31:02 -0600

    added #pragma warning restore

--- Workspace.cs
+++ Workspace.cs
@@ -6,6 +6,8 @@
     int problemNumber = 0; // The ID of the current problem you're working on. When you've completed your current problem, increment this by one.
     bool displayProblem = true; // Should CodingRange display problem details?
     bool evaluateAnswer = true; // Should CodingRange run & evaluate Method?
+
+    #pragma warning restore
     #endregion
 
     private string Method() // This method MUST be called Method (case-sensitive) for CodingRange to pick it up.
